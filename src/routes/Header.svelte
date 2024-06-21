d922e936f35fc233e1b0db87813932ae8f379cd6
Anton <metallist9656@gmail.com>
2024-06-21 10:03:35 -0500

    fix(): updated links

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
@@ -13,11 +14,11 @@
 
 	<nav>
 		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+			<li aria-current={$page.url.pathname === `${base}/` ? 'page' : undefined}>
+				<a href="{base}/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === `${base}/about` ? 'page' : undefined}>
+				<a href="{base}/about">About</a>
 			</li>
 		</ul>
 	</nav>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,11 +6,6 @@
 <div class="text-column">
 	<h1>About this app</h1>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
 	<pre>npm create svelte@latest</pre>
 
 	<p>
