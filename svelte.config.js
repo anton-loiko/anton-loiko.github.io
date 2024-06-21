48430216e949b9f2f95187368020e1b28a8d1ff1
Anton <metallist9656@gmail.com>
2024-06-21 09:48:06 -0500

    chore(): moved to top handleHttpError

--- svelte.config.js
+++ svelte.config.js
@@ -4,15 +4,6 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
 		prerender: {
 			handleHttpError: ({ path, referrer, message, referenceType, status }) => {
 				console.log(path, '<=== path ===');
@@ -24,6 +15,16 @@ const config = {
 				// otherwise fail the build
 				throw new Error(message);
 			}
+		},
+
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	},
 	preprocess: vitePreprocess()
