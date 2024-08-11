7ad4bf7640c283ddd426cb9655f5f3bb89374bc2
Anton Loiko <anton.loiko.engineer@gmail.com>
2024-08-11 09:10:06 -0500

    build(): add specific adapter for Vercel

--- src/routes/blog/categories/[slug]/+page.ts
+++ src/routes/blog/categories/[slug]/+page.ts
@@ -7,7 +7,7 @@ export const csr = dev;
 
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-export const prerender = false;
+export const prerender = true;
 
 export async function load({ fetch, params }) {
 	const response = await fetch('/api/posts');
