d87b0471c548214ca8175001d0fd206d3d639d33
Anton Loiko <anton.loiko.engineer@gmail.com>
2024-08-15 12:09:16 -0500

    feat(ABOUT): Update about section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,42 +11,22 @@
 	<DownloadResume isIconOnly={false} className="mb-6" />
 
 	<p class="font-roboto text-white mb-4 text-sm md:text-base">
-		Passionate Front-End Developer with 6 years of experience in building engaging user interfaces
-		and enhancing user experience through innovative JavaScript solutions. Proficient in React,
-		TypeScript, JavaScript, Angular, and Vue.js. Committed to continuous learning and improvement,
-		always exploring new technologies to stay at the forefront of the industry. Looking for new
-		challenges to apply my expertise and contribute to impactful projects.
+		As a Senior Frontend Engineer and Fullstack Developer, I bring over 6 years of experience in
+		crafting dynamic and responsive user interfaces. My passion lies in UI/UX design, where I merge
+		creativity with technical prowess to build engaging, user-friendly applications.
 	</p>
 
 	<p class="font-roboto text-white mb-4 text-sm md:text-base">
-		From my work, I expect continuous professional growth and development opportunities. I notice
-		leadership qualities in myself, such as advocating for my team members and organizing team
-		efforts.
+		Currently, I am deeply immersed in C++ and Unreal Engine, exploring the intricate world of game
+		development and UI programming. My journey in tech has been driven by a relentless curiosity and
+		a commitment to continuous learning, which has led me to work on diverse projects from mobile
+		apps to complex web systems.
 	</p>
 
 	<p class="font-roboto text-white mb-4 text-sm md:text-base">
-		I strive to work in a dynamic and innovative environment where I can apply my knowledge and
-		experience to solve complex problems and create valuable products. Working with a team of
-		professionals who value collaborative effort and knowledge sharing is important to me.
-	</p>
-
-	<p class="font-roboto text-white mb-4 text-sm md:text-base">
-		I also value a balance between work and personal life, so having a flexible schedule and
-		maintaining a healthy work atmosphere are crucial. I prefer to avoid monotonous tasks and thrive
-		in environments that allow for creativity and innovation. Honest and transparent communication
-		within the team and with management is also important to me.
-	</p>
-
-	<p class="font-roboto text-white mb-4 text-sm md:text-base">
-		What I do not want from work is limited opportunities for professional growth, lack of learning
-		and development opportunities. I do not want to work in an environment where support and team
-		interaction are absent, and where the opinions and ideas of each team member are not valued.
-		Additionally, I aim to avoid roles that do not offer opportunities for creativity and personal
-		growth.
-	</p>
-
-	<p class="font-roboto text-white mb-4 text-sm md:text-base">
-		In summary, I am seeking a role that fosters my growth, allows me to contribute to team success,
-		and enables me to develop both professionally and personally.
+		I'm excited about opportunities that allow me to leverage my skills in frontend and fullstack
+		development, while also diving deeper into the realm of game development. Let's connect if
+		you're looking for someone who is not only technically adept but also passionate about creating
+		exceptional user experiences.
 	</p>
 </Container>
